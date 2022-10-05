366acf0f4a91d69c000fa32f503eb09879f0dd96
jhaysonj <jhaysonbj@dcc.ufrj.br>
2022-10-04 21:24:29 -0400

    atualizando

--- calcula_xor.py
+++ calcula_xor.py
@@ -2,7 +2,7 @@
 ip = 0b11000000101010000000000000001010
 
 # representacao da mascara em binario
-mascara = 0b11111111111111111111111100000000
+mascara = 0b11111111111111111111111111000000
 
 # resultado do and bit a bit entre o ip e a mascara
 resultado_and_binario = bin(ip&mascara)
